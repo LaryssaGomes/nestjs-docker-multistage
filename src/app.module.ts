47b8fbf4f74059887e0ee0a1f044b3e6e786c1be
Laryssa Gomes <Laryssag533@gmail.com>
2026-09-03 14:24:37 -0300

    feat: adicionar configuração de ambiente e suporte ao NestJS Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'mydatabase',
-      entities: [],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
+        entities: [],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
